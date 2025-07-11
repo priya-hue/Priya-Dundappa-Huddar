96aa9128f782dbe39bd7c28efe390fb918aae070
priya-hue <60704045+priya-hue@users.noreply.github.com>
2025-07-11 09:06:38 +0530

    Update Program-4.java

--- Program-4.java
+++ Program-4.java
@@ -5,7 +5,7 @@ public class MultiplesCount
         int[] numbers = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
 
         
-        for (int i = 1; i <= 9; i++) 
+        for (int i = 1; i <= 9; i++)
         {
             int count = 0;
 
