933992d98666022f994f4b9c33d57a766ec1e708
priya-hue <60704045+priya-hue@users.noreply.github.com>
2025-07-11 08:46:25 +0530

    Update Program-2.java

--- Program-2.java
+++ Program-2.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class OddNumberSeries {
+    public static void main(String[] args) {
+        // Input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number (a): ");
+        int a = scanner.nextInt();
+
+        int currentOdd = 1;
+        System.out.print("Output: ");
+        for (int i = 1; i <= a; i++) {
+            System.out.print(currentOdd);
+            if (i < a) {
+                System.out.print(", ");
+            }
+            currentOdd += 2;
+        }
+    }
+}
 
